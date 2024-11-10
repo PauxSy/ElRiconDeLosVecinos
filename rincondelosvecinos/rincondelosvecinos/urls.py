1dbb7d633c02d82f973d38a6145cd30f61ad075c
PauxSy <paula.salgado03@inacapmail.cl>
2024-11-10 15:38:43 -0300

    Update 10-11 Fran

--- rincondelosvecinos/rincondelosvecinos/urls.py
+++ rincondelosvecinos/rincondelosvecinos/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.vista_catalogo, name='catalogo'),  
-    path('iniciouser/', views.vista_iniciouser, name='iniciouser'), 
+    path('', views.vista_catalogo, name='catalogo'), 
+    path('registrouser/', views.vista_registrouser, name='registrouser'),
+    path('iniciouser/', views.vista_iniciouser, name='iniciouser'),
+    path('perfiluser/', views.vista_perfiluser, name='perfiluser'),
+    path('historialuser/', views.vista_historialuser, name='historialuser'),
+
 ]
--- rincondelosvecinos/rincondelosvecinos/views.py
+++ rincondelosvecinos/rincondelosvecinos/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
+# ---------------usuario---------------- 
+
 def vista_catalogo(request):
     return render(request, 'catalogo.html')
 
+def vista_registrouser(request):
+    return render(request,'registroUser.html')
+
 def vista_iniciouser(request):
     return render(request, 'inicioSesioónUser.html')
+
+def vista_perfiluser(request):
+    return render(request,'perfilUser.html')
+
+def vista_historialuser(request):
+    return render(request,'historialCompraUser.html')
+
+
+# ---------------Admin---------------- 
+
+def vista_
+
+
+
